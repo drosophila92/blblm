55e50f6cd6a4bdd5ee72f5921e268737804b52bd
Yige <ygeluo@ucdavis.edu>
2021-03-10 18:59:21 -0800

    update the R wrapper to fastwLm.cpp to handle zero weights

--- R/fastwLm.R
+++ R/fastwLm.R
@@ -6,7 +6,41 @@ fastwLm <- function(X, y, w) {
   # stopifnot(is.matrix(X), is.numeric(y), is.numeric(w),
   #          nrow(y)==nrow(X), nrow(y)==nrow(w)  )
 
+  ny <- NCOL(y)
+  zero.weights <- any( w == 0 )
+  if (zero.weights) {
+    save.r <- y
+    save.w <- w
+    ok <- w != 0
+    nok <- !ok
+    w <- w[ok]
+    X0 <- X[!ok, , drop = FALSE]
+    X <- X[ok, , drop = FALSE]
+    n <- nrow(X)
+    y0 <- if (ny > 1L)
+      y[!ok, , drop = FALSE]
+    else y[!ok]
+    y <- if (ny > 1L)
+      y[ok, , drop = FALSE]
+    else y[ok]
+  }
+
+
   fit <- .Call('_blblm_fastwLm_impl', PACKAGE = 'blblm', X, y, w)
+
+  coef <- fit$coefficients
+
+  if (zero.weights) {
+    coef[is.na(coef)] <- 0
+    f0 <- X0 %*% coef
+
+    save.r[ok] <- fit$residuals
+    save.r[nok] <- y0 - f0
+
+    fit$residuals <- save.r
+    fit$weights <- save.w
+  }
+
   names(fit$coefficients) <- colnames(X)
   #names(fit$residuals) <- rownames(X)
   fit
